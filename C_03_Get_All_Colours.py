977e0eb6b0cf32dc9a2017196559db1ed0fcbb47
DarkSnap <100321435+DarkSnap@users.noreply.github.com>
2025-02-19 05:40:01 +0900

    Integrated Colour Retrieval

--- C_03_Get_All_Colours.py
+++ C_03_Get_All_Colours.py
@@ -1,9 +1,45 @@
 import csv
 import random
 
+def round_ans(val):
+    """
+    Rounds numbers to nearest integer
+    :param val: number to be rounded.
+    :return: Rounded number (an integer)
+    """
+    var_rounded = (val * 2 + 1) // 2
+    raw_rounded = f"{var_rounded:.0f}"
+    return int(raw_rounded)
+
 # Retrieve colours from csv file and put them in a list
 file = open("00_colour_list_hex_v3.csv", "r")
-all_colors = list(csv.reader(file, delimiter=","))
+all_colours = list(csv.reader(file, delimiter=","))
 file.close()
 
-print(all_colors)
+# Remove the first row
+all_colours.pop(0)
+
+round_colours = []
+colour_scores = []
+
+# Loop until we have four colours with different scores...
+while len(round_colours) < 4:
+    potential_colour = random.choice(all_colours)
+
+    # Get the score and check it's not a duplicate
+    if potential_colour[1] not in colour_scores:
+        round_colours.append(potential_colour)
+        colour_scores.append(potential_colour[1])
+
+print(all_colours)
+print(colour_scores)
+
+# Find target score (medium)
+
+# Chance scores to integers
+int_scores = [int(x) for x in colour_scores]
+int_scores.sort()
+
+median = (int_scores[1] + int_scores[2]) / 2
+median = round_ans(median)
+print("Median", median)
